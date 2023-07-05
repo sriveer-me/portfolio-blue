052de66721be8905a663c84ea652e17765ff67dc
veeru-neerukonda <veeru.neerukonda@gmail.com>
2023-07-05 10:35:58 +0530

    feat:project initialized

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+const root = document.getElementById('root');
+const reactRoot = ReactDOM.createRoot(root);
+reactRoot.render(<App />);
